0f889decf72cb7e76687f7b6c4b91a742326479b
joshshipton <shiptonjosh@gmail.com>
2023-12-05 18:34:17 +0800

    stuff

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -9,7 +9,7 @@
   let loading = true;
   onMount(async () => {
     // wait 2 seconds
-    // await new Promise(r => setTimeout(r, 2000));
+    // await new Promise((r) => setTimeout(r, 2000));
     loading = false;
   });
 </script>
@@ -25,25 +25,64 @@
       </div>
     </div>
   {:else}
+    <div class="absolute left-0 px-4 py-4 hover:-translate-y-2">
+      <svg
+        width="47"
+        height="50"
+        viewBox="0 0 47 50"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        class="hover:text-black w-10 h-10"
+      >
+        <path
+          d="M19.8135 45.6633H9.80888C6.56709 45.6515 3.93867 43.0328 3.91485 39.7911V19.8906C3.92122 18.8053 4.40682 17.7784 5.24155 17.0849L7.00324 15.78C7.64639 15.0947 7.71179 14.0491 7.15903 13.2891C6.60628 12.529 5.59139 12.2691 4.74132 12.6698L2.93614 14.0183C1.1152 15.4272 0.0346042 17.5884 0 19.8906V39.8128C0.011961 45.2251 4.39655 49.6097 9.80888 49.6217H19.8135C20.8946 49.6217 21.7709 48.7453 21.7709 47.6643C21.7709 46.5832 20.8946 45.7068 19.8135 45.7068V45.6633Z"
+          fill="#959595"
+        />
+        <path
+          d="M47 19.8471C46.9771 17.5618 45.9317 15.4071 44.1509 13.9748L29.1222 1.99097C25.8196 -0.663656 21.1151 -0.663656 17.8126 1.99097L13.0495 5.44909C12.1546 6.09172 11.9502 7.33811 12.5928 8.23298C13.2354 9.12786 14.4818 9.33235 15.3767 8.68972L20.205 5.07935C22.1001 3.51914 24.8347 3.51914 26.7298 5.07935L41.7585 17.0632C42.6234 17.7488 43.1345 18.7869 43.1504 19.8906V39.8128C43.1504 43.056 40.5213 45.6851 37.2781 45.6851H32.8195C32.3631 45.6851 31.9931 45.3151 31.9931 44.8586V35.8762C31.9931 33.0139 29.6791 30.6901 26.8168 30.6781H20.292C18.9134 30.6781 17.5912 31.2258 16.6164 32.2006C15.6416 33.1754 15.0939 34.4976 15.0939 35.8762V39.3126C15.0939 40.3936 15.9703 41.27 17.0514 41.27C18.1324 41.27 19.0088 40.3936 19.0088 39.3126V35.8762C19.003 35.5379 19.1333 35.2114 19.3705 34.9701C19.6077 34.7288 19.9319 34.593 20.2703 34.593H26.795C27.4917 34.593 28.0565 35.1578 28.0565 35.8545V44.8586C28.0684 47.4807 30.1974 49.5999 32.8195 49.5999H37.1694C42.5867 49.5999 46.9783 45.2084 46.9783 39.7911L47 19.8471Z"
+          fill="#959595"
+        />
+      </svg>
+    </div>
+    <div class="absolute right-0 px-4 py-4 hover:-translate-y-2">
+      <svg
+        width="575"
+        height="634"
+        viewBox="0 0 575 634"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        class=" w-10 h-10"
+      >
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M419.747 51.149C387.543 18.394 342.097 0.333344 289.067 0.333344C236.037 0.333344 190.591 18.394 158.389 51.149C126.212 83.8787 108.548 129.964 108.548 183.667C108.548 255.967 140.027 302.567 178.987 330.54C186.558 335.973 194.383 340.69 202.277 344.757C158.738 356.72 123.386 377.577 94.6031 403.447C49.0907 444.35 20.8824 496.977 1.87773 544.1C-1.56494 552.637 2.5644 562.347 11.1014 565.79C19.6381 569.233 29.3491 565.103 32.7917 556.567C50.8807 511.713 76.7154 464.34 116.884 428.237C156.65 392.5 211.336 367 289.067 367C361.733 367 414.193 389.283 453.133 421.263C492.4 453.51 518.843 496.387 537.79 538.773C550.43 567.053 528.247 600.333 493.423 600.333H108.829C99.6247 600.333 92.1627 607.797 92.1627 617C92.1627 626.203 99.6247 633.667 108.829 633.667H493.423C548.493 633.667 592.263 578.96 568.223 525.173C548.107 480.173 519.013 432.23 474.29 395.503C447.743 373.703 416.01 356.123 378.01 345.357C398.333 334.12 415.04 319.207 428.333 302.303C457.097 265.733 469.587 220.339 469.587 183.667C469.587 129.964 451.923 83.8787 419.747 51.149ZM395.977 74.5177C421.147 100.121 436.253 137.37 436.253 183.667C436.253 213.661 425.803 251.6 402.133 281.697C378.917 311.217 342.69 333.667 289.067 333.667C267.38 333.667 229.963 326.103 198.428 303.46C167.75 281.433 141.881 244.701 141.881 183.667C141.881 137.37 156.988 100.121 182.159 74.5177C207.305 48.9393 243.783 33.6667 289.067 33.6667C334.35 33.6667 370.83 48.9393 395.977 74.5177Z"
+          fill="black"
+        />
+      </svg>
+    </div>
+
     <div class="flex flex-col items-center justify-center h-screen">
       <div
-        class="bg-gray-100 dark:bg-gray-800 rounded-lg border bg-card text-card-foreground shadow-sm w-full max-w-3xl hover:shadow"
+        class="bg-gray-100 dark:bg-gray-800 rounded-lg border bg-card text-card-foreground shadow-sm w-full max-w-6xl hover:shadow"
         data-v0-t="card"
       >
         <div class=" hover:bg-gray-400 px-2 py-2">Feedback</div>
         <div class="flex flex-col space-y-1.5 p-6">
-          <p class="flex flex-row justify-center">2x + 4 = 0 (solve for x)</p>
+          <p class="flex flex-row justify-center text-slate-600 font-[Virgil]">2x + 4 = 0 (solve for x)</p>
         </div>
         <div class="p-6 divide-y divide-gray-200">
           <div transition:slide class="transition duration-300 ease-in-out">
-            <div class="flex flex-row justify-start items-center w-full space-x-4 hover:shadow-lg">
-              <div class="ml-4 px-6 text-orange-500 font-bold ">2x = -4</div>
+            <div
+              class="flex flex-row justify-start items-center w-full space-x-4 hover:shadow-lg"
+            >
+              <div class="ml-4 px-6 text-orange-500 font-bold font-[Virgil]">2x = -4</div>
 
-              <div class="py-2  rounded px-4 flex-grow">
+              <div class="py-2 rounded px-4 flex-grow">
                 <div class="flex items-center justify-between">
                   <div class="text-sm font-medium">Part 1</div>
                   <div
-                    class="bg-green-600 inline-flex items-center rounded-full border px-2.5 py-0.5 font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 text-sm"
+                    class=" border-green-600 text-green-600 inline-flex items-center rounded-full border px-2.5 py-0.5 font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 text-sm"
                   >
                     Correct
                   </div>
@@ -56,59 +95,56 @@
             </div>
           </div>
           <div transition:slide class="transition duration-300 ease-in-out">
-            <div class="flex flex-row justify-start items-center w-full space-x-4 hover:shadow-lg">
-              <div class="ml-4 px-6 text-orange-500 font-bold ">2x = -4</div>
+            <div
+              class="flex flex-row justify-start items-center w-full space-x-4 hover:shadow-lg"
+            >
+              <div class="ml-4 px-6 text-orange-500 font-bold font-[Virgil]">x = -4/2</div>
 
-              <div class="py-2  rounded px-4 flex-grow">
+              <div class="py-2 rounded px-4 flex-grow">
                 <div class="flex items-center justify-between">
                   <div class="text-sm font-medium">Part 2</div>
                   <div
-                  class="bg-red-600 inline-flex items-center rounded-full border px-2.5 py-0.5 font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 text-sm"
-                >
-                  Incorrect
-                </div>
-
+                    class="border-red-600 text-red-600 inline-flex items-center rounded-full border px-2.5 py-0.5 font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 text-sm"
+                  >
+                    Incorrect
+                  </div>
                 </div>
                 <div class="mt-1 text-sm text-gray-500 dark:text-gray-400">
-                    You then divided both sides by negative 2 which is incorrect,
-                    you should divide both sides by 2
+                  You then divided both sides by negative 2 which is incorrect,
+                  you should divide both sides by 2
                 </div>
               </div>
             </div>
           </div>
 
           <div transition:slide class="transition duration-300 ease-in-out">
-            <div class="flex flex-row justify-start items-center w-full space-x-4 hover:shadow-lg">
-              <div class="ml-4 px-6 text-orange-500 font-bold ">2x = -4</div>
+            <div
+              class="flex flex-row justify-start items-center w-full space-x-4 hover:shadow-lg"
+            >
+              <div class="ml-4 px-6 text-orange-500 font-bold font-[Virgil]">x = -2</div>
 
-              <div class="py-2  rounded px-4 flex-grow">
+              <div class="py-2 rounded px-4 flex-grow">
                 <div class="flex items-center justify-between">
                   <div class="text-sm font-medium">Part 3</div>
                   <div
-                    class="bg-green-600 inline-flex items-center rounded-full border px-2.5 py-0.5 font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 text-sm"
+                    class=" border-green-600 text-green-600 inline-flex items-center rounded-full border px-2.5 py-0.5 font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 text-sm"
                   >
                     Correct
                   </div>
                 </div>
                 <div class="mt-1 text-sm text-gray-500 dark:text-gray-400">
-                    Following on from your previous step you solved for x, this is
-                    correct
+                  Following on from your previous step you solved for x, this is
+                  correct
                 </div>
               </div>
             </div>
           </div>
+        </div>
 
-        </div>
-        <div>
-          <div class="flex items-center justify-between p-4">
-            <div class="text-sm font-medium">Total</div>
-            <div class="text-sm font-medium">2/3</div>
-          </div>
-        </div>
         <div class="flex items-center p-4 flex-col">
           <div class="font-medium">Advice for next time</div>
           <div>
-            <div class="text-sm text-gray-500 dark:text-gray-400">
+            <div class="text-sm text-gray-500 dark:text-gray-400 justify-center px-20">
               You should divide both sides by 2 instead of -2. You don't need to
               change the sign when dividing or multiplying algebraic
               expressions.
@@ -116,19 +152,31 @@
           </div>
         </div>
 
+        <div>
+          <div class="flex items-center flex-row justify-center space-x-6 p-4">
+            <div class="text-sm font-bold">Total</div>
+            <div class="text-sm font-bold">2/3</div>
+          </div>
+        </div>
+
         <div class="p-4">
           <button
-            class=" hover:bg-slate-700 inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 w-full"
+            class="hover:bg-slate-700 inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 w-full"
           >
             Next Question
           </button>
         </div>
       </div>
     </div>
+    
+
+
   {/if}
 </body>
 
 <style>
+
+  
   body {
     background-image: url("../images/grid.jpg");
     background-size: cover;
@@ -170,5 +218,13 @@
     100% {
       transform: rotate(360deg);
     }
+  }
+
+  p{
+    font-size: 2rem;
+  }
+
+  div{
+    font-size: 1rem;
   }
 </style>
